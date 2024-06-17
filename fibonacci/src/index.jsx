6fbaa1a52afc4ed0554ac441284f7e3254124e4f
unknown <davimoreiraasantoss@gmail.com>
2024-06-17 20:53:00 -0300

    organized the folders

---
+++ fibonacci/src/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import styles from './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <main className={`${styles.a} text-primary`}>
+    <h1 className={`text-danger`}>Belongs to Fibonacci ?</h1>
+    <p>Type a number to check if belongs to Fibonacci sequence</p>
+    <input type="text" />
+  </main>
+);
+
