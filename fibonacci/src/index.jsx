cd394fc65e90e77dfb4363cf074abc91f56fbda8
unknown <davimoreiraasantoss@gmail.com>
2024-06-20 23:09:16 -0300

    fixed the erros with npm start by deleting node_modules and typing 'npm i'

---
+++ fibonacci/src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import styles from './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <main className={`${styles.a}`}>
+    <h1 className={`text-danger`}>Belongs to Fibonacci ?</h1>
+    <p>Type a number to check if belongs to Fibonacci sequence</p>
+    <input type="text" />
+  </main>
+);
+
+export default root
