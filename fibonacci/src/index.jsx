cdd9e24bcbc9b8420e76f2dc05d1f431af1342ad
unknown <davimoreiraasantoss@gmail.com>
2024-06-21 22:53:27 -0300

    now bootstrap works

--- fibonacci/src/index.jsx
+++ fibonacci/src/index.jsx
@@ -4,7 +4,7 @@ import styles from './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <main className={`${styles.a} d-none`}>
+  <main className={`${styles.a} `}>
     <h1 className={`text-danger`}>Belongs to Fibonacci ?</h1>
     <p>Type a number to check if belongs to Fibonacci sequence</p>
     <input type="text" />
