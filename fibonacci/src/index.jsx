90553987b2608ef54f8f532e5eaf2faecf8d00f9
unknown <davimoreiraasantoss@gmail.com>
2024-06-21 22:38:44 -0300

    update

--- fibonacci/src/index.jsx
+++ fibonacci/src/index.jsx
@@ -4,7 +4,7 @@ import styles from './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <main className={`${styles.a}`}>
+  <main className={`${styles.a} d-none`}>
     <h1 className={`text-danger`}>Belongs to Fibonacci ?</h1>
     <p>Type a number to check if belongs to Fibonacci sequence</p>
     <input type="text" />
