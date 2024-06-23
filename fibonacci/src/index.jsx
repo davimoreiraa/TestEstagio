074607d218c10ae04f37b5aebef1bb55a1401b88
unknown <davimoreiraasantoss@gmail.com>
2024-06-23 11:33:40 -0300

    now belongsToF is only called when you click on the button

--- fibonacci/src/index.jsx
+++ fibonacci/src/index.jsx
@@ -5,15 +5,15 @@ import styles from './index.css';
 import React, { useState } from 'react';
 
 export default function Index() {
-  const [num, setNum] = useState('')
-  const [nums, setNums] = useState([0,1])
+  const [num, setNum] = useState()
+  // const [nums, setNums] = useState([0,1])
     
   function belongsToF(numChosen) {
-    while (numChosen > nums[nums.length -1]) {
-        setNums(...nums (nums[nums.length -1] + nums[nums.length -2]))
-    }
-    console.log(nums)
-    nums.includes(numChosen) ? console.log('O número existe na Sequência Fibonacci') : console.log('O número não existe na Sequência Fibonacci')
+    // while (numChosen > nums[nums.length -1]) {
+    //     setNums((nums[nums.length -1] + nums[nums.length -2]))
+    // }
+    console.log('a')
+    // nums.includes(numChosen) ? console.log('O número existe na Sequência Fibonacci') : console.log('O número não existe na Sequência Fibonacci')
   }
 
     return (
@@ -22,12 +22,12 @@ export default function Index() {
           <h1 className={``}>Belongs to Fibonacci ?</h1>
           <p> {num} Type a number to check if belongs to Fibonacci sequence</p> 
           <input 
-          type="number"
+          type="text"
           onKeyUp={(e) => {
             setNum(e.target.value)
           }}
           />
-          <button onClick={belongsToF(num)}>Check</button> 
+          <button onClick={() => belongsToF(num)}>Check</button>
         </div>
       </main>
     )
