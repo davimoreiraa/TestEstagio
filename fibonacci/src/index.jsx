00e2600fe64c1011df8d17c24aab2d56698d693e
unknown <davimoreiraasantoss@gmail.com>
2024-06-23 10:39:17 -0300

    fixed the useState import

--- fibonacci/src/index.jsx
+++ fibonacci/src/index.jsx
@@ -2,22 +2,34 @@
 import styles from './index.css';
 
 /* ------------------  RESOURCES -------------------*/
-import React from 'react';
-// import { useState } from 'react';
+import React, { useState } from 'react';
 
-
-export default function index() {
-    // const [num, setNum] = useState('')
+export default function Index() {
+  const [num, setNum] = useState('')
+  const [nums, setNums] = useState([0,1])
     
+  function belongsToF(numChosen) {
+    while (numChosen > nums[nums.length -1]) {
+        setNums(...nums (nums[nums.length -1] + nums[nums.length -2]))
+    }
+    console.log(nums)
+    nums.includes(numChosen) ? console.log('O número existe na Sequência Fibonacci') : console.log('O número não existe na Sequência Fibonacci')
+  }
+
     return (
       <main className={`${styles.a} d-flex justify-content-center align-items-center`}>
         <div className={``}>
           <h1 className={``}>Belongs to Fibonacci ?</h1>
-          <p>Type a number to check if belongs to Fibonacci sequence</p>
-          <input type="text" />
+          <p> {num} Type a number to check if belongs to Fibonacci sequence</p> 
+          <input 
+          type="number"
+          onKeyUp={(e) => {
+            setNum(e.target.value)
+          }}
+          />
+          <button onClick={belongsToF(num)}>Check</button> 
         </div>
       </main>
-
     )
 
 }
