aaef99580bf78e0a866cfba4b24af50b54bf38f8
unknown <davimoreiraasantoss@gmail.com>
2024-06-23 22:11:07 -0300

    updat styles

--- fibonacci/src/index.jsx
+++ fibonacci/src/index.jsx
@@ -1,23 +1,24 @@
 /* ------------------  STYLES -------------------*/
-import styles from './index.css';
+import styles from './styles.module.css'
 
 /* ------------------  RESOURCES -------------------*/
 import React, { useState } from 'react';
 
 export default function Index() {
   const [num, setNum] = useState()
-  // const [nums, setNums] = useState([0,1])
+  const [nums, setNums] = useState([0,1])
     
   function belongsToF(numChosen) {
-    // while (numChosen > nums[nums.length -1]) {
-    //     setNums((nums[nums.length -1] + nums[nums.length -2]))
-    // }
-    console.log('a')
-    // nums.includes(numChosen) ? console.log('O número existe na Sequência Fibonacci') : console.log('O número não existe na Sequência Fibonacci')
+    while (numChosen > nums[nums.length -1]) {
+      setNums(nums)
+        nums.push(((nums[nums.length -1] + nums[nums.length -2])))
+    }
+    console.log(nums)
+    nums.includes(numChosen) ? console.log('O número existe na Sequência Fibonacci') : console.log('O número não existe na Sequência Fibonacci')
   }
 
     return (
-      <main className={`${styles.a} d-flex justify-content-center align-items-center`}>
+      <main className={`${styles.container} d-flex justify-content-center align-items-center`}>
         <div className={``}>
           <h1 className={``}>Belongs to Fibonacci ?</h1>
           <p> {num} Type a number to check if belongs to Fibonacci sequence</p> 
