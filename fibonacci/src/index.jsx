94ea739a73f4059bb363f807c5dc8030ce65a54e
unknown <davimoreiraasantoss@gmail.com>
2024-06-26 21:50:14 -0300

    update index

--- fibonacci/src/index.jsx
+++ fibonacci/src/index.jsx
@@ -7,10 +7,11 @@ import React, { useState } from 'react';
 export default function Index() {
   const [num, setNum] = useState()
   const [nums, setNums] = useState([0,1])
+  const [message, setMessage] = useState('')
     
   function belongsToF(numChosen) {
-    if(typeof numChosen !== Number) {
-      console.log('Insert a valid number')
+    if(typeof numChosen !== 'number') {
+      setMessage('Insert a valid number')
       return
     }
       
@@ -19,23 +20,32 @@ export default function Index() {
         nums.push(((nums[nums.length -1] + nums[nums.length -2])))
     }
     
-    nums.includes(numChosen) ? console.log('O número existe na Sequência Fibonacci') : console.log('O número não existe na Sequência Fibonacci')
+    nums.includes(numChosen) ? setMessage('The number exists in Fibonacci sequence') : setMessage("The number doesn'texists in Fibonacci sequence")
   }
 
+    /* ------------------ PAGE -------------------*/
     return (
-      <main className={`${styles.container} d-flex justify-content-center align-items-center`}>
-        <div className={``}>
-          <h1 className={``}>Belongs to Fibonacci ?</h1>
-          <p> {num} Type a number to check if belongs to Fibonacci sequence</p> 
-          <input 
-          type="text"
-          onKeyUp={(e) => {
-            setNum(parseInt(e.target.value))
-          }}
-          />
-          <button onClick={() => belongsToF(num)}>Check</button>
-        </div>
-      </main>
+      <div className={`${styles.height}`}>
+        <main className={`${styles.container} d-flex justify-content-center align-items-center`}>
+          <div className={``}>
+            <h1 className={``}>Belongs to Fibonacci ?</h1>
+            <p> Type a number to check if belongs to Fibonacci sequence</p> 
+            <div className={`d-flex justify-content-center align-items-center`}>
+              <input 
+              className={`me-2`}
+              type="text"
+              onKeyUp={(e) => {
+                setNum(parseInt(e.target.value))
+              }}
+              />  
+            <button className={`btn btn-secondary`} onClick={() => belongsToF(num)}>Check</button>
+            </div>
+            <div className={`d-flex justify-content-center mt-3`}>
+              {message}
+            </div>
+            </div>
+        </main>
+      </div>
     )
 
 }
